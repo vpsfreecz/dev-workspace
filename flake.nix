1fb732c7f96dfc24332fbe3f3f4df08a2906e2e3
Jakub Skokan <jakub.skokan@havefun.cz>
2026-09-13 00:11:11 +0200

    Update dev-workspace for portal queue recovery

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    dev-workspace.url = "github:aither64/dev-workspace/ca4ae9feea2aa4767948ad7146ab9fc71daf0e1d";
+    dev-workspace.url = "github:aither64/dev-workspace/7b5c46e02355b4352c1ad1d7497603adb5182cd9";
     devcluster-vpsadminos.url = "github:vpsfreecz/vpsadminos/15802517e2d92dda4ddc07ebac3d1d7ea087b430";
     devcluster-vpsadmin = {
       url = "github:vpsfreecz/vpsadmin/master";
